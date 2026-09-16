3806b2446f53360d669f73dec28c1e3d59af5340
jotarampini-cell <jotarampini@gmail.com>
2026-09-16 14:09:55 -0400

    Corregir formato de precios (escapar variables)

---
+++ fix.py
@@ -0,0 +1,13 @@
+﻿import re
+
+with open('index.html', 'r', encoding='utf-8') as f:
+    content = f.read()
+
+content = content.replace(',550', '$2,550')
+content = content.replace(',315', '$3,315')
+content = content.replace('un sobrecargo de por prioridad', 'un sobrecargo de $765 por prioridad')
+
+with open('index.html', 'w', encoding='utf-8') as f:
+    f.write(content)
+
+print("Fixed")
